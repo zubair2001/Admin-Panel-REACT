ec6af7fb64e9923fe951a6fd723d5677f5185d3d
zubair2001 <87979294+zubair2001@users.noreply.github.com>
2022-12-29 21:21:12 +0500

    4 files modified, 8 added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 import './home.scss';
+import Widget from '../../components/widget/Widget';
+import Featured from '../../components/featured/Featured';
+import Chart from '../../components/chart/Chart';
+import Table from '../../components/table/Table';
 
 const Home = () => {
   return (
@@ -9,6 +13,20 @@ const Home = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table />
+        </div>
       </div>
     </div>
   );
